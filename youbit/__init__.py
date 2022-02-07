f4f03d297d9d456973009489f0d99aeae402fbb1
Florian Laporte <85995217+MeViMo@users.noreply.github.com>
2022-02-07 21:28:14 +0100

    Create __init__.py

---
+++ youbit/__init__.py
@@ -0,0 +1,9 @@
+"""
+YouBit: using YouTube as a very poor free file hosting service.
+"""
+__title__= "youbit"
+__version__ = "0.1.0"
+__author__ = "Florian Laporte"
+__license__ = "MIT License"
+
+from youbit.__main__ import Encoder, Decoder
