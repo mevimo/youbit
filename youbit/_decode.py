56afd9f88bda2d9b4d5962e251d3d6c0abb2cdc6
MeViMo <florianlap2@gmail.com>
2022-06-01 19:58:30 +0200

    cleanup of encode.py and decode.py

--- youbit/_decode.py
+++ youbit/_decode.py
@@ -1,168 +1,19 @@
-import cv2
-import subprocess
 import numpy as np
-from numba import njit, prange
-import time
+from numba import njit
 
 
-
-# @njit('void(uint8[::1], uint8[::1], uint8[::1])', inline='never')
-# def _numba_transform_bpp3_x64(arr, out, mapping):
-#     for i in range(64):
-#         j = i * 3
-#         out[i] = mapping[(arr[j]<<2)|(arr[j+1]<<1)|(arr[j+2])]
-
-
-# @njit('void(uint8[::1], uint8[::1])')
-# def _numba_read_bpp3(arr, out):
-#     for i in range(arr.size):
-#         if arr[i] < 32:
-#             out[i] = 0
-#         if 32 <= arr[i] < 64:
-#             out[i] = 1
-#         if 64 <= arr[i] < 96:
-#             out[i] = 2
-#         if 96 <= arr[i] < 128:
-#             out[i] = 3
-#         if 128 <= arr[i] < 160:
-#             out[i] = 4
-#         if 160 <= arr[i] < 192:
-#             out[i] = 5
-#         if 192 <= arr[i] < 224:
-#             out[i] = 6
-#         if 224 <= arr[i]:
-#             out[i] = 7
-
-
-# @njit('void(uint8[::1], uint8[::1])')
-# def _numba_read_bpp3(arr, out):
-#     for i in range(arr.size):
-#         j = i * 3
-#         if arr[i] < 32:
-#             out[j] = 0
-#             out[j+1] = 0
-#             out[j+2] = 0
-#         if 32 <= arr[i] < 64:
-#             out[j] = 0
-#             out[j+1] = 0
-#             out[j+2] = 1
-#         if 64 <= arr[i] < 96:
-#             out[j] = 0
-#             out[j+1] = 1
-#             out[j+2] = 0
-#         if 96 <= arr[i] < 128:
-#             out[j] = 0
-#             out[j+1] = 1
-#             out[j+2] = 1
-#         if 128 <= arr[i] < 160:
-#             out[j] = 1
-#             out[j+1] = 0
-#             out[j+2] = 0
-#         if 160 <= arr[i] < 192:
-#             out[j] = 1
-#             out[j+1] = 0
-#             out[j+2] = 1
-#         if 192 <= arr[i] < 224:
-#             out[j] = 1
-#             out[j+1] = 1
-#             out[j+2] = 0
-#         if 224 <= arr[i]:
-#             out[j] = 1
-#             out[j+1] = 1
-#             out[j+2] = 1
-
-# @njit('void(uint8[::1], uint8[::1], uint8[::1])', inline='never')
-# def _numba_transform_bpp3_x64(arr, out, mapping):
-#     for i in range(64):
-#         j = i * 3
-#         out[i] = mapping[(arr[j]<<2)|(arr[j+1]<<1)|(arr[j+2])]
-
-
-# @njit('void(uint8[::1], int_, uint8[::1], uint8[::1])', parallel=True)
-# def _numba_transform_bpp3(arr, div, out, mapping):
-#     for i in prange(div//64):
-#         _numba_transform_bpp3_x64(arr[i*192:(i*192)+192], out[i*64:(i*64)+64], mapping)
-
-
-@njit('void(uint8[::1], uint8[::1])', inline='never')
-def _numba_pack_x64(arr, out):
-    for i in range(64):
-        j = i * 8
-        out[i] = (arr[j]<<7)|(arr[j+1]<<6)|(arr[j+2]<<5)|(arr[j+3]<<4)|(arr[j+4]<<3)|(arr[j+5]<<2)|(arr[j+6]<<1)|arr[j+7]
-
-       
-@njit('void(uint8[::1], uint8[::1])', parallel=True)
-def _numba_pack(arr, out):
-    for i in prange(out.size//64):
-        _numba_pack_x64(arr[i*512:(i*512)+512], out[i*64:(i*64)+64])
-    # for i in range(div//64*64, div):
-    #     j = i * 8
-    #     su[i] = (arr[j]<<7)|(arr[j+1]<<6)|(arr[j+2]<<5)|(arr[j+3]<<4)|(arr[j+4]<<3)|(arr[j+5]<<2)|(arr[j+6]<<1)|arr[j+7]
-    
-
-# @njit('void(uint8[::1], uint8[::1], bool_[::1])', inline='never')
-# def _numba_read_bpp3_x64(arr, out, mapping):
-#     for i in range(64):
-#         j = i * 3
-#         idx = (arr[i] * 3) >> 5
-#         out[j] = mapping[idx]
-#         out[j+1] = mapping[idx+1]
-#         out[j+2] = mapping[idx+2]
-
-
-# @njit('void(uint8[::1], uint8[::1])', parallel=True)
-# def _numba_read_bpp2(arr, out):
-#     mapping = np.array([0,0, 0,1, 1,0, 1,1], dtype=np.bool_)
-#     # mapping1 = np.array([0, 0, 1, 1], dtype=np.bool_)
-#     # mapping2 = np.array([0, 1, 0, 1], dtype=np.bool_)
-#     # for i in prange(arr.size):
-#     #     j = i * 2
-#     #     idx = arr[i] >> 6
-#     #     out[j] = mapping1[idx]
-#     #     out[j+1] = mapping2[idx]
-#     for i in prange(arr.size):
-#         j = i * 2
-#         idx = (arr[i] >> 6) * 2
-#         out[j] = mapping[idx]
-#         out[j+1] = mapping[idx+1]
-
-
-
-# @njit('void(uint8[::1], uint8[::1])', inline='never')
-# def _numba_read_bpp2_x128(arr, out):
-#     for i in range(128):
-#         j = i * 4
-#         out[i] = (arr[j] & 192) | (arr[j+1] & 192 >> 2) | (arr[j+2] & 192 >> 4) | (arr[j+3] & 192 >> 6)
-
-
-
-# @njit('void(uint8[::1], uint8[::1])', parallel=True)
-# def _numba_read_bpp2(arr, out):
-#     for i in prange(out.size):
-#         idxarr = i*512
-#         idxout = i*128
-#         _numba_read_bpp2_x128(arr[idxarr:idxarr+512], out[idxout:idxout+128])
+@njit('void(uint8[::1])')
+def _numba_read_bpp1(arr):
+    for i in range(len(arr)):
+        arr[i] = arr[i] >> 7
 
 
 @njit('void(uint8[::1], uint8[::1])')
 def _numba_read_bpp2(arr, out):
-    #! cant get this to scale meaningfully with parallelism... is far from the bottleneck either way
+    #! cant get this ( and any of the numba accelerated functions) to scale meaningfully with parallelism... is far from the bottleneck either way
     for i in range(out.size):
         j = i * 4
         out[i] = (arr[j] & 192) | (arr[j+1] & 192 >> 2) | (arr[j+2] & 192 >> 4) | (arr[j+3] & 192 >> 6)
-    
-
-# @njit('void(uint8[::1], uint8[::1])', parallel=True)
-# def _numba_read_bpp3(arr, out):
-#     mapping = np.array([0,0,0, 0,0,1, 0,1,0, 0,1,1, 1,0,0, 1,0,1, 1,1,0, 1,1,1], dtype=np.bool_)
-#     # for i in prange(arr.size // 64):
-#     #     _numba_read_bpp3_x64(arr[i*64:(i*64)+64], out[i*192:(i*192)+192], mapping)
-#     for i in prange(arr.size):
-#         j = i * 3
-#         idx = (arr[i] >> 5) * 3
-#         out[j] = mapping[idx]
-#         out[j+1] = mapping[idx+1]
-#         out[j+2] = mapping[idx+2]
     
 
 @njit('void(uint8[::1], uint8[::1])')
@@ -176,12 +27,6 @@ def _numba_read_bpp3(arr, out):
         out[out_i+2] = out_x3 & 255
 
 
-@njit('void(uint8[::1])')
-def _numba_read_bpp1(arr):
-    for i in range(len(arr)):
-        arr[i] = arr[i] >> 7
-
-
 def read_pixels(arr: np.ndarray, bpp: int) -> np.ndarray:
     """Expects a numpy ndarray of datatype uint8.
     Each element is expected to represent an entire pixel.
@@ -190,102 +35,10 @@ def read_pixels(arr: np.ndarray, bpp: int) -> np.ndarray:
         _numba_read_bpp1(arr)
         out = np.packbits(arr)
     elif bpp == 2:
-        # out = np.zeros(arr.size*2, dtype=np.uint8)
-        # _numba_read_bpp2(arr, out)
-        # out = np.packbits(out)
         out = np.zeros(arr.size//4, dtype=np.uint8)
         _numba_read_bpp2(arr, out)
     elif bpp == 3:
-        # out = np.zeros(arr.size*3, dtype=np.uint8)
-        # _numba_read_bpp3(arr, out)
-        # out = np.packbits(out)
         out_size = int(arr.size * 3 / 8)
         out = np.zeros(out_size, dtype=np.uint8)
         _numba_read_bpp3(arr, out)
     return out
-
-
-
-
-
-
-
-
-
-
-
-
-
-def extract_frames(tempdir) -> None:
-    file = tempdir.glob('*.mp4').send(None)
-    cmd = "ffmpeg -i {} -r 1/1 {}/frame%d.png".format( # 1 frame captures per second, which leave 2 unusable frame in beginning for unknown reasons
-        str(file), str(tempdir))
-    subprocess.Popen(cmd, shell=True).wait()
-    # Remove video file
-    file.unlink()
-
-
-def extract_binary(tempdir, par_parity_size: int, par_recovery_size: int, bpp: int) -> None:
-    '''assumes frameé".png naming blahblha'''
-    ## TODO: extract key frames only for much less error rate, but I have to figure out YouTube's GOP structure first
-    # 12th frame is keyframe duplicate, as well as 29th frame and 46th frame... distance of 17 frames with first dupe-key-frame at 12?
-    # my 1fps is being transformed into a higher fps, maybe thats why?
-    # might be a coincidental correlation....
-    frame_paths = sorted(
-        tempdir.glob('*.png'),
-        key = lambda p : int(p.stem[5:])
-        # key=os.path.getmtime
-        )
-    frames = []
-
-    if bpp == 1:
-        for f in frame_paths:
-            if f.name in ('frame1.png', 'frame2.png'): ## code duplication, remove  from frames var before this code block
-                continue
-            img = cv2.imread(str(f), cv2.IMREAD_GRAYSCALE)
-            pixels = np.array(img, dtype=np.uint8)
-            del img
-            pixels[pixels < 128] = 0
-            pixels[pixels > 127] = 1
-            frames.append(np.packbits(pixels))
-            del pixels
-    elif bpp == 2:
-        ##TODO
-        pass
-    elif bpp == 3:
-        for f in frame_paths:
-            if f.name in ('frame1.png', 'frame2.png'):
-                continue
-            pixels = cv2.imread(str(f), cv2.IMREAD_GRAYSCALE)
-            pixels[pixels < 32] = 0 #000
-            pixels[(pixels >= 32) & (pixels < 64)] = 1 #001
-            pixels[(pixels >= 64) & (pixels < 96)] = 2 #010
-            pixels[(pixels >= 96) & (pixels < 128)] = 3 #011
-            pixels[(pixels >= 128) & (pixels < 160)] = 4 #100
-            pixels[(pixels >= 160) & (pixels < 192)] = 5 #101
-            pixels[(pixels >= 192) & (pixels < 224)] = 6 #110
-            pixels[pixels >= 224] = 7 #111
-            pixels = np.unpackbits(pixels).reshape(-1,8)
-            pixels = np.delete(pixels, [0,1,2,3,4], 1).ravel()
-            frames.append(np.packbits(pixels))
-            del pixels
-    
-    bin = np.concatenate(frames)
-    del frames
-
-    par_parity_path = tempdir / 'bin.par2'
-    bin[:par_parity_size].tofile(par_parity_path)
-    par_recovery_path = tempdir / 'bin.vol0+0.par2'
-    bin[par_parity_size:par_recovery_size].tofile(par_recovery_path)
-    par_main_path = tempdir / 'bin'
-    bin[par_recovery_size:].tofile(par_main_path)
-    del bin
-
-
-# def remove_ecc(tempdir):
-#     ##TODO: switch to par2.exe binary 
-#     subprocess.Popen(f'par2deep-cli -q -clean -dir {str(tempdir)}', shell=True).wait() #wont work because working directory of subprocess is not yet set to the youbit/youbit
-
-#     for file in tempdir.glob('*.par2'):
-#         file.unlink()
-
--- youbit/_encode.py
+++ youbit/_encode.py
@@ -1,64 +1,5 @@
 import numpy as np
-from pathlib import Path
 from numba import njit, prange
-
-
-##TODO: Disable more filters @ video encoder
-##TODO: chunking....
-
-
-def apply_ecc_par(tempdir: Path) -> tuple[Path, int, int]:
-    """
-    Will create a parity file, and a file with recovery blocks in the given directory,
-    using par2.
-    PC argument specifies the percentage of data BLOCKS that may be corrupted before
-    data can no longer be recreated.
-    """
-    # par2deep was designed for CLI use, not like this, hence the weirdness.
-    # This tool is TEMPORARY, it is very limited since i can only set a 'percentage'
-    # for error resiliency, but I have no control over block size.
-    # Block size is set to whatever creates a source block count of 2000, with 72 as a minimum.
-    # This means block size can easily become massive for larger files, which is okay for bad
-    # sectors on a HDD, but not particulary great for the kind of small error streaks we see
-    # in this application.
-    # I might have to consider a non library multi-platform application with CLI support,
-    # just ship youbit with the binaries, and interface with it via subprocess.Popen()...
-    ## TODO: switch to par2.exe subprocess
-    # par = par2deep(str(tempdir))
-    # par.args['quiet'] = True
-    # par.args['percentage'] = pc
-    # par.args['clean_backup'] = True
-    # par.check_state()
-    # for _ in par.execute(): pass
-
-    # # -----------------------------------------------------------
-    # for file in tempdir.iterdir():
-    #     if 'vol' in file.name and file.suffix == '.par2':
-    #         par_recovery = file
-    #         par_recovery_size = os.path.sizeof(par_recovery)
-    #     elif file.suffix == '.par2':
-    #         par_parity = file
-    #         par_parity_size = os.path.sizeof(par_parity)
-    #     else:
-    #         par_main = file
-    # with open(par_parity, 'ab') as parity, open(par_recovery, 'rb') as recovery, open(par_main, 'rb') as main:
-    #     # We want it in order; parity -> recovery -> main
-    #     shutil.copyfileobj(recovery, parity)
-    #     shutil.copyfileobj(main, parity)
-    # file = par_parity.rename(par_parity.parent / 'file.bin')
-    # par_recovery.unlink()
-    # par_main.unlink()
-    # return file, par_parity_size, par_recovery_size
-
-
-def apply_ecc_polar():
-    ##TODO: cython extension for aff3ct lib, if possible
-    pass
-
-
-def apply_ecc():
-    #TODO: interface with go library, pretty much only choice
-    pass
 
 
 def add_lastframe_padding(arr: np.ndarray, res: tuple[int, int], bpp: int) -> np.ndarray:
@@ -90,6 +31,8 @@ def _numba_transform_bpp2_x64(arr, out, mapping):
 
 
 @njit('void(uint8[::1], int_, uint8[::1], uint8[::1])', parallel=True)
+#! remove the loop unrolling, div, mapping, floor division, and all the bloat that is unnecessary. Also, it should be tested again if this benefits from parallelism, because i doubt it.
+#! should prob be done when i make (made) the tests for it to verify we did not break everything.
 def _numba_transform_bpp2(arr, div, out, mapping):
     for i in prange(div//64):
         _numba_transform_bpp2_x64(arr[i*128:(i*128)+128], out[i*64:(i*64)+64], mapping)
@@ -142,102 +85,3 @@ def transform_array(arr: np.ndarray, bpp: int) -> np.ndarray:
     else: # should probably be inn api class not here cause well have it twice then
         raise ValueError(f'Unsupported bpp argument: {bpp} of type {type(bpp)}.')
     return out
-
-
-# class VideoEncoder:
-#     """1920x1080"""
-#     def __init__(self, output: Path, framerate: int, res: tuple[int, int], crf: int):
-#         ##TODO: validate crf to be within possible values
-#         ##TODO: raise error when output file path already exists, unless 'overwrite' parameter or something is set to true
-#         self.container = av.open(str(output), mode='w')
-#         self.stream = self.container.add_stream("libx264", rate=framerate)
-#         self.stream.width = res[0]
-#         self.stream.height = res[1]
-#         self.stream.options = {'crf':str(crf), 'tune': 'grain', '-x264opts': 'no-deblock'}
-#     def feed(self, arr):
-#         ##TODO: validation, arr.reshape should work, if that works the rest of feed() works too. validate or is native numpy error clear enough?
-#         arr = arr.reshape(-1, self.stream.height, self.stream.width)
-#         for framearr in arr:
-#             frame = av.VideoFrame.from_ndarray(framearr, format='gray')
-#             self.container.mux(self.stream.encode(frame))
-#     def finish(self):
-#         self.container.mux(self.stream.encode())
-#         self.container.close()
-
-
-# def video_encode(arr: np.ndarray, output: Path, framerate: int, res: tuple[int, int], crf: int) -> None:
-#     """1920x1080"""
-#     arr = arr.reshape(-1, res[1], res[0])
-#     container = av.open(str(output), mode='w')
-#     stream = container.add_stream("libx264", rate=framerate)
-#     stream.width = res[0]
-#     stream.height = res[1]
-#     stream.options = {'crf':str(crf), 'tune': 'grain', '-x264opts': 'no-deblock'}
-#     # stream.options = {'crf': '20', 'tune': 'grain', '-x264opts': 'no-deblock'}
-#     for framearr in arr:
-#         frame = av.VideoFrame.from_ndarray(framearr, format='gray')
-#         container.mux(stream.encode(frame))
-#     container.mux(stream.encode())
-#     container.close()
-
-    
-# def encode(file: Path, output: Path, bpp: int = 3, framerate: int = 1, crf: int = 0) -> None:
-#     """IDK EXPLAIN
-
-#     :param file: Path object of the file in question.
-#     :type file: Path
-#     :param bpp: 'Bits Per Pixel', how many bits to encode per
-#         pixel. The higher the value, the higher the chance of
-#         corruption during compression.
-#     :type bpp: int
-#     :raises ValueError: if the value of the 'bpp' argument is
-#         not currently supported.
-#     """
-#     if bpp == 1:
-#         mapping = np.array([0,255], dtype=np.uint8)
-#     elif bpp == 2:
-#         mapping = np.array([0,160,96,255], dtype=np.uint8)
-#     elif bpp == 3:
-#         # mapping = np.array([0,144,80,208,48,176,112,255], dtype=np.uint8)
-#         mapping = np.array([0,48,80,112,144,176,208,255], dtype=np.uint8)
-#     else:
-#         raise ValueError('Unsupported BPP (Bits Per Pixel) value.')
-
-#     start = time.time()
-#     print(f'start: {start}')
-#     bin = np.fromfile(file, dtype=np.uint8)
-#     tok = time.time()
-#     print(f'Finished loading file: {time.time() - start}')
-#     framesize = 259200 * bpp
-#     last_frame_padding = framesize - (bin.size % framesize)
-#     if last_frame_padding:
-#         bin = np.append(bin, np.zeros(last_frame_padding, dtype=np.uint8))
-#     framecount = int(bin.size / framesize)
-
-#     print(f'finished padding, starting unpackbits: {time.time() - start}')
-#     bin = np.unpackbits(bin)
-#     print(f'Unpacked bits: {time.time() - start}')
-#     if bpp > 1:
-#         bin = np.packbits(bin, axis=1, bitorder='little').ravel()
-#         # bin = numba_packbits(bin)
-#     print(f'packed bits: {time.time() - start}')
-#     bin = mapping[bin]
-#     print(f'FINISHED MAPPING: {time.time() - start}')
-#     bin = bin.reshape(framecount, 1080, 1920)
-
-#     print(f'finished reshape, starting container.... {time.time() - start}')
-#     container = av.open(str(output), mode='w')
-#     stream = container.add_stream("libx264", rate=framerate)
-#     stream.width = 1920
-#     stream.height = 1080
-#     stream.options = {'crf':str(crf), 'tune': 'grain', '-x264opts': 'no-deblock'}
-#     # stream.options = {'crf': '20', 'tune': 'grain', '-x264opts': 'no-deblock'}
-#     print(f'Starting encoding loop: {time.time() - start}')
-#     for binframe in bin:
-#         frame = av.VideoFrame.from_ndarray(binframe, format='gray')
-#         container.mux(stream.encode(frame))
-#     print(f'Finished encoding loop, closing container now: {time.time() - start}')
-#     container.mux(stream.encode())
-#     container.close()
-
-
